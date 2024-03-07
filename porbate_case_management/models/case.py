ba22bb83e50bf29983f494dfcacb725fa12af17b
abadawizss <ahmad.badawi@zensoftware.us>
2024-03-07 18:29:43 +0700

    update

--- porbate_case_management/models/case.py
+++ porbate_case_management/models/case.py
@@ -12,23 +12,42 @@ class CaseStage(models.Model):
     sequence = fields.Integer('Sequence')
     name = fields.Char('Stage Name')
 
+class Parties(models.Model):
+    _name = 'probate.case.parties'
+
+    case_id = fields.Many2one('probate.case', string='case')
+    name = fields.Char('Name')
+    email = fields.Char('Email')
+    phone = fields.Char('Phone')
+
+class Parties(models.Model):
+    _name = 'probate.case.beneficiaries'
+
+    case_id = fields.Many2one('probate.case', string='case')
+    name = fields.Char('Name')
+    email = fields.Char('Email')
+    phone = fields.Char('Phone')
+
 class ProbateCase(models.Model):
     _name = 'probate.case'
     _description = 'Probate Case'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Case Number', default="New Case")
+    case_number = fields.Char('Case Number')
+    name = fields.Char(string='System Case Number', compute='_get_system_number')
     court_id = fields.Many2one('probate.case.court', string='Court Name')
     district_id = fields.Many2one('probate.case.district', string='District')
     branch_district_id = fields.Many2one('probate.case.branch.district', string='Branch District')
     presiding_magistrate = fields.Many2one('res.partner', string='Presiding Magistrate')
-    parties_involved = fields.Many2many(comodel_name='res.partner', relation='parties_involved_rel', string='Parties Involved')
-    beneficiaries = fields.Many2many(comodel_name='res.partner', relation='beneficiaries', string='Beneficiaries')
+    phone = fields.Char('Phone', related='presiding_magistrate.phone')
+    email = fields.Char('Email', related='presiding_magistrate.email')
+    parties_involved = fields.One2many('probate.case.parties', 'case_id', string='Parties Involved', ondelete='cascade')
+    beneficiaries = fields.One2many('probate.case.beneficiaries', 'case_id', string='Beneficiaries', ondelete='cascade')
     case_property_ids = fields.One2many('probate.case.property', 'case_id', string='Properties', ondelete="cascade")
-    administrator_name = fields.Many2one('res.users', string='Administrator Name', default=lambda self: self.env.user,)
+    administrator_name = fields.Many2one('res.users', string='Name of the Administrator of the estate', default=lambda self: self.env.user,)
     deceased_name = fields.Char(string='Deceased Name')
     completion_date = fields.Date(string='Completion Date')
-    administrator_phone = fields.Char(string='Administrator Phone')
+    administrator_phone = fields.Char(string='Administrators phone number')
     company_id = fields.Many2one('res.company', string="Company", default=lambda self: self.env.user.company_id.id, index=1)
     currency_id = fields.Many2one('res.currency', 'Currency', compute='_compute_currency_id')
     total_value = fields.Float('Total', store=True, readonly=True, compute='_amount_all')
@@ -41,32 +60,51 @@ class ProbateCase(models.Model):
         ('closed', 'Closed'),
     ], string='state', default='draft')
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get('name', 'New Case') == 'New Case':
-                date = fields.Date.today()
-                code_district = self.env['probate.case.district'].search([('id','=', int(vals.get('district_id')))]).district_code
-                code_branch_district = self.env['probate.case.branch.district'].search([('id','=', int(vals.get('branch_district_id')))]).branch_district_code
-                date_today = date.strftime('%d/%m/%Y')
+    @api.depends('case_number', 'district_id', 'branch_district_id')
+    def _get_system_number(self):
+        for record in self:
+            record.name = ''
+            if record.case_number and not record.district_id and not record.branch_district_id:
+                sequence = record.case_number
+                record.update({
+                    'name': sequence,
+                })
+            if record.case_number and record.district_id and not record.branch_district_id:
+                sequence = record.district_id.district_code + ' ' +record.case_number 
+                record.update({
+                    'name': sequence,
+                })
+            if record.case_number and record.district_id and record.branch_district_id:
+                sequence = record.district_id.district_code + '/' + record.branch_district_id.branch_district_code +  ' ' + record.case_number 
+                record.update({
+                    'name': sequence,
+                })
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for vals in vals_list:
+    #         if vals.get('name', 'New Case') == 'New Case':
+    #             date = fields.Date.today()
+    #             code_district = self.env['probate.case.district'].search([('id','=', int(vals.get('district_id')))]).district_code
+    #             code_branch_district = self.env['probate.case.branch.district'].search([('id','=', int(vals.get('branch_district_id')))]).branch_district_code
+    #             date_today = date.strftime('%d/%m/%Y')
                 
-                pattern = code_district + '/' + code_branch_district + ' ' + '%' + '/' + date_today
-                last_seq_number_rec = self.env['probate.case'].sudo().search([
-                        ('name', 'like', pattern)
-                    ], order='id desc', limit=1)
+    #             pattern = code_district + '/' + code_branch_district + ' ' + '%' + '/' + date_today
+    #             last_seq_number_rec = self.env['probate.case'].sudo().search([
+    #                     ('name', 'like', pattern)
+    #                 ], order='id desc', limit=1)
 
-                if last_seq_number_rec and last_seq_number_rec.name:
-                    last_seq_number_match = re.search(r'\s(\d{2})/', last_seq_number_rec.name)
-                    if last_seq_number_match:
-                        last_seq_number = int(last_seq_number_match.group(1))
-                        sequence_number = last_seq_number + 1
-                    else:
-                        sequence_number = 1
-                else:
-                    sequence_number = 1
+    #             if last_seq_number_rec and last_seq_number_rec.name:
+    #                 last_seq_number_match = re.search(r'\s(\d{2})/', last_seq_number_rec.name)
+    #                 if last_seq_number_match:
+    #                     last_seq_number = int(last_seq_number_match.group(1))
+    #                     sequence_number = last_seq_number + 1
+    #                 else:
+    #                     sequence_number = 1
+    #             else:
+    #                 sequence_number = 1
 
-                vals['name'] = code_district + '/' + code_branch_district + ' ' + str(sequence_number).zfill(2) + '/' + date_today
-            return super().create(vals)
+    #             vals['name'] = code_district + '/' + code_branch_district + ' ' + str(sequence_number).zfill(2) + '/' + date_today
+    #         return super().create(vals)
 
 
     def action_submit(self):
